2a3db7604c089851c77326e4cf0c907f0e0065e6
Anoir Boudoudouh <anoir2@live.it>
2024-02-24 20:37:54 +0100

    Created dimension table loader operator

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -8,15 +8,39 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 table,
+                 redshift_connection_id,
+                 insert_sql_query,
+                 truncate_before_insert,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_connection_id = redshift_connection_id
+        self.insert_sql_query = insert_sql_query
+        self.truncate_before_insert = truncate_before_insert
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_connection_id)
+        if self.truncate_before_insert:
+            self.log.info(f"Truncating dimension table {self.table}")
+            sql = f"TRUNCATE TABLE {self.table};COMMIT;"
+            self.__execute_sql(redshift, sql)
+        else:
+            self.log.info(f"Truncate disabled in the table {self.table}. Append-only mode enabled.")
+
+
+        self.log.info(f"Loading dimension table {self.table}")
+        sql = f"""INSERT INTO {self.table} 
+                    {self.insert_sql_query}; 
+                    COMMIT;"""
+        self.__execute_sql(redshift, sql)
+        self.log.info(f"Dimension table {self.table} loaded successfully.")
+
+    
+    def __execute_sql(self, redshift, sql):
+        try:
+            redshift.run(sql)
+        except Exception as e:
+            self.log.error("Error on executing query. Query", sql," The error is: ", e)
+            raise e
