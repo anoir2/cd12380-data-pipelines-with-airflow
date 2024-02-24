a308b30d16db060801a8aaab613f49207c330720
Anoir Boudoudouh <anoir2@live.it>
2024-02-24 20:36:47 +0100

    Created stage table loader operator

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,26 +1,46 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
-                 *args, **kwargs):
-
+        table="",
+        aws_credentials_id="",
+        redshift_connection_id="",
+        s3_bucket="",
+        s3_path= "",
+        json_path="",
+        *args, **kwargs):
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.aws_credentials_id = aws_credentials_id
+        self.redshift_connection_id = redshift_connection_id
+        self.s3_bucket = s3_bucket
+        self.s3_path = s3_path
+        self.json_path = json_path
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        s3_uri = f"s3://{self.s3_bucket}/{self.s3_path}"
 
+        self.log.info(f"Loading {self.table} staging table from {s3_uri}")
+        
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_connection_id)
 
+        staging_events_copy = f"""
+            copy {self.table}
+            from '{s3_uri}'
+            ACCESS_KEY_ID '{credentials.access_key}'
+            SECRET_ACCESS_KEY '{credentials.secret_key}'
+            json '{self.json_path}'
+            COMPUPDATE OFF;
+                    """
+        
+        redshift.run(staging_events_copy)
 
-
-
+        self.log.info(f"Staging table {self.table} loaded successfully.")
